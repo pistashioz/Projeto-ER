b21cf7959f0dfdda55a474261f5dca4c585a7abc
Victoria Martinez <martinezvictoria781@gmail.com>
2023-05-23 14:00:37 +0100

    music settings done

--- interface/script.js
+++ interface/script.js
@@ -7,8 +7,10 @@ muteBtn.addEventListener('click', muteOrUnmute, false);
 function muteOrUnmute(){
     if (myTrack.muted == true){
         myTrack.muted = false;
+        muteBtn.innerHTML = '<ion-icon name="volume-high-outline"></ion-icon>'
     }
     else{
         myTrack.muted = true;
+        muteBtn.innerHTML = '<ion-icon name="volume-mute-outline"></ion-icon>'
     }
 }
