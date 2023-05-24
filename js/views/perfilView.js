9803b2417e0a0754cbc7cb5437c4f6a315650a6d
Victoria Martinez <martinezvictoria781@gmail.com>
2023-05-24 22:59:16 +0100

    dom edit and save data

--- js/views/perfilView.js
+++ js/views/perfilView.js
@@ -1,3 +1,5 @@
+/*modal*/
+
 const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
@@ -10,3 +12,19 @@ const toggleModal = () => {
 [openModalButton, closeModalButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+/*DOM edit and save data*/
+
+const editBtn = document.querySelector('#settings');
+const saveBtn = document.querySelector('#save');
+
+editBtn.addEventListener('click', function(){
+    document.getElementById('userName').removeAttribute('readonly');
+    document.getElementById('email').removeAttribute('readonly');
+
+});
+
+saveBtn.addEventListener('click', function(){
+    document.getElementById('userName').setAttribute('readonly', true);
+    document.getElementById('email').setAttribute('readonly', true);
+})
