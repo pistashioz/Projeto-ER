0184657fd5358df5e2923c315ac355406f374343
Victoria Martinez <martinezvictoria781@gmail.com>
2023-05-27 20:27:05 +0100

    need to improve it

--- js/views/perfilView.js
+++ js/views/perfilView.js
@@ -1,45 +1,65 @@
 /*profile modal*/
-
+const cbtn = document.querySelector('#modalRul');
 const openModalButton = document.querySelector("#open-modalProfile");
-const closeModalButton = document.querySelector("#close-modalProfile");
+const closeModalButton = document.querySelectorAll(".close-modal");
 const modal = document.querySelector("#modalProfile");
 const fade = document.querySelector("#fade");
+const toggleModal=0;
+const rModal = document.querySelector("#modalRules");
+modal.addEventListener('click', function(){
+  if(!rModal.classList.contains('hide')){ 
+    modal.style.zIndex=12;
+    rModal.style.zIndex=10;
+    document.getElementById('modalPro').style.zIndex =12;
 
-const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-};
+  }
+  else {
+    modal.classList.toggle('hide');
+      }
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
 });
 
+closeModalButton.forEach((x) => x.addEventListener('click',[rModal,modal].forEach(y=>y.classList.add('hide'))));
+closeModalButton.forEach(x=> x.addEventListener('click',console.log('gogo')))
 
-/*rules modal*/ 
-const openRModalButton = document.querySelector("#open-modalRules");
-const closeRModalButton = document.querySelector("#close-modalRules");
-const Rmodal = document.querySelector("#modalRules");
+rModal.addEventListener('click', function(){
+  if (rModal.classList.contains('hide') && modal.classList.contains('hide')){
+    [rModal,modal].forEach((x) => x.classList.remove('hide'))
+    
+    cbtn.style.zIndex = 13;
+    modal.style.zIndex=10;
+    rModal.style.zIndex=13;
+  }
+  else if (rModal.classList.contains('hide') &&   !(modal.classList.contains('hide'))){
+    modal.classList.toggle('hide')
+    rModal.classList.toggle('hide')
+    rModal.style.zIndex = 12;
+    cbtn.style.zIndex = 13;
+    
+  }
+  else{
+    modal.style.zIndex=10;
+    rModal.style.zIndex = 12;
+    cbtn.style.zIndex = 13;
+    [rModal,modal].forEach((x) => x.classList.add('hide'));
 
-const toggleRModal = () => {
-    [Rmodal, fade].forEach((el) => el.classList.toggle("hide"));
-};
-
-[openRModalButton, closeRModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleRModal());
-});
-
+} 
+    
+})
 
 /*exit modal*/
 
-const openEModalButton = document.querySelector("#open-modalExit");
-const closeEModalButton = document.querySelector("#close-modalExit");
-const Emodal = document.querySelector("#containerExit");
+const openeModalButton = document.querySelector("#open-modalExit");
+const closeeModalButton = document.querySelector("#close-modalExit");
+const eModal = document.querySelector("#containerExit");
 
-const toggleEModal = () => {
-    [Emodal, fade].forEach((el) => el.classList.toggle("hide"));
+const toggleeModal = () => {
+    [eModal, fade].forEach((el) => el.classList.toggle("hide"));
+    [rModal, modal].forEach((el) => el.classList.add("hide"));
 };
 
-[openEModalButton, closeEModalButton, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleEModal());
+[openeModalButton, closeeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleeModal());
 });
 
 /*DOM edit and save profile data*/
@@ -56,4 +76,4 @@ editBtn.addEventListener('click', function(){
 saveBtn.addEventListener('click', function(){
     document.getElementById('userName').setAttribute('readonly', true);
     document.getElementById('email').setAttribute('readonly', true);
-})
+})  
