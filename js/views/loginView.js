64e0955b995d3c3828315f214d121091d27b6db0
GoncaloDuarte06 <goncalomsduarte06@gmail.com>
2023-06-16 00:41:34 +0100

    Adjust in music

--- js/views/loginView.js
+++ js/views/loginView.js
@@ -5,18 +5,18 @@ import * as User from "../models/UserModels.js";
 
 var muteBtn = document.getElementById('sound');
 var myTrack = document.getElementById('bgTrack');
-muteBtn.addEventListener('click', muteOrUnmute, false);
 
-function muteOrUnmute(){
-    if (myTrack.muted == true){
-        myTrack.muted = false;
-        muteBtn.innerHTML = '<ion-icon name="volume-high-outline"></ion-icon>'
-    }
-    else{
-        myTrack.muted = true;
-        muteBtn.innerHTML = '<ion-icon name="volume-mute-outline"></ion-icon>'
-    }
+myTrack.volume = 0.1;
+muteBtn.addEventListener('click', ()=>{
+  if (myTrack.muted == true){
+    myTrack.muted = false;
+    muteBtn.innerHTML = '<ion-icon name="volume-high-outline"></ion-icon>'
 }
+else{
+    myTrack.muted = true;
+    muteBtn.innerHTML = '<ion-icon name="volume-mute-outline"></ion-icon>'
+}
+});
 
 /*log in*/
 
--- js/views/roomView.js
+++ js/views/roomView.js
@@ -543,3 +543,19 @@ if (user.getUserLevel() > 2){
 
 
 
+
+var muteBtn = document.getElementById('sound');
+var myTrack = document.getElementById('bgTrack');
+
+myTrack.volume = 0.1;
+
+muteBtn.addEventListener('click', ()=>{
+  if (myTrack.muted == true){
+    myTrack.muted = false;
+    muteBtn.innerHTML = '<ion-icon name="volume-high-outline"></ion-icon>'
+}
+else{
+    myTrack.muted = true;
+    muteBtn.innerHTML = '<ion-icon name="volume-mute-outline"></ion-icon>'
+}
+});
